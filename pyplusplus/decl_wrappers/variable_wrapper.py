987ab17f2d5f067f104f58bceaf81bac48f66d1f
roman_yakovenko <none@none>
2006-05-02 05:04:56 +0000

    adding exportable functionality

--- pyplusplus/decl_wrappers/variable_wrapper.py
+++ pyplusplus/decl_wrappers/variable_wrapper.py
@@ -10,3 +10,17 @@ class variable_t(decl_wrapper.decl_wrapper_t, declarations.variable_t):
     def __init__(self, *arguments, **keywords):
         declarations.variable_t.__init__(self, *arguments, **keywords )
         decl_wrapper.decl_wrapper_t.__init__( self )
+        
+    def _exportable_impl( self ):
+        if not isinstance( self.parent, declarations.class_t ):
+            return ''
+        if self.bits == 0 and self.name == "":
+            return "pyplusplus can not expose alignement bit."
+        type_ = declarations.remove_const( self.type )
+        if declarations.is_pointer( type_ ):
+            if self.type_qualifiers.has_static:
+                return "pyplusplus, right now, can not expose static pointer member variables. This could be changed in future."
+            if declarations.is_fundamental( type_.base ):
+                return "pyplusplus, right now, can not expose pointer to fundamental member variables. This could be changed in future."
+        return ''
+    
