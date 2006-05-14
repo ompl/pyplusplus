554d085e0560c473fd844aa520089d32d5a0bc60
roman_yakovenko <none@none>
2006-05-14 08:14:24 +0000

    changing gccxml path

--- environment.py
+++ environment.py
@@ -37,7 +37,7 @@ if 'roman' in getpass.getuser():
         boost.libs = '/home/roman/boost_cvs/bin'
         boost.include = '/home/roman/boost_cvs'
         python.include = '/usr/include/python2.4'
-        gccxml.executable = '/home/roman/gccxml/bin/gccxml'
+        gccxml.executable = '/home/roman/gccxml-build/bin/gccxml'
 
 
 _my_path = None
