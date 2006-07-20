7e06ef7fd0e9f8faffbc15ad1a97f13a12521a2d
roman_yakovenko <none@none>
2006-07-20 05:21:56 +0000

    testing new interface

--- unittests/module_body_tester.py
+++ unittests/module_body_tester.py
@@ -17,8 +17,13 @@ class tester_t(fundamental_tester_base.fundamental_tester_base_t):
             , tester_t.EXTENSION_NAME
             , *args )
 
+    def customize(self, mb):
+        mb.build_code_creator( self.EXTENSION_NAME )
+        mb.add_declaration_code( "int get1(){ return 1;} ")
+        mb.add_registration_code( 'bp::def( "get1", &get1 );' )
+
     def run_tests(self, module):        
-        pass
+        self.failUnless( 1 == module.get1() )
     
 def create_suite():
     suite = unittest.TestSuite()    
