e94820c5f85a3e00ea60c1ee20527cafcd5fe484
roman_yakovenko <none@none>
2006-07-20 19:12:15 +0000

    adding documentation for documentation strings

--- docs/documentation/www_configuration.py
+++ docs/documentation/www_configuration.py
@@ -2,4 +2,6 @@ name = 'documentation'
 main_html_file = 'index.html'
 files_to_skip = ['indexing_suite_v2.html']
 names = { 'containers' : 'STL containers'
-          , 'how_to' : 'how to ... ?' }
+          , 'how_to' : 'how to ... ?'
+          , 'doc_string' : 'documentation string'
+}
