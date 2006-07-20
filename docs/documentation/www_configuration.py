c746207c8e5eb3d6bc66905920c7414974cfd609
roman_yakovenko <none@none>
2006-07-20 06:50:38 +0000

    adding "how to ... ?" to documentation

--- docs/documentation/www_configuration.py
+++ docs/documentation/www_configuration.py
@@ -1,3 +1,5 @@
-name = 'C++ containers'
-main_html_file = 'containers.html'
+name = 'documentation'
+main_html_file = 'index.html'
 files_to_skip = ['indexing_suite_v2.html']
+names = { 'containers' : 'STL containers'
+          , 'how_to' : 'how to ... ?' }
