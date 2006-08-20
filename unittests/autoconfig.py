d74f0ff744fa9d5064a7d31d03562f11a4a6641d
roman_yakovenko <none@none>
2006-08-20 07:45:50 +0000

    update autoconfig.py to use new pydsc API

--- unittests/autoconfig.py
+++ unittests/autoconfig.py
@@ -21,9 +21,9 @@ from environment import scons, boost, python, gccxml
 class scons_config:
     libs = ['boost_python']
     libpath = [ boost.libs, python.libs ]
-    cpppath = [ boost.include, python.include ]    
+    cpppath = [ boost.include, python.include ]
     include_dirs = cpppath + [data_directory]
-    
+
     @staticmethod
     def create_sconstruct():
         code = [
@@ -37,19 +37,62 @@ class scons_config:
           , "    , SHLIBSUFFIX='%s'" % scons.suffix #explicit better then implicit
           , ")" ]
         return os.linesep.join( code )
-    
-#I need this in order to allow Python to load just compiled modules    
+
+#I need this in order to allow Python to load just compiled modules
 sys.path.append( build_dir )
 
 os.chdir( build_dir )
 
-if sys.platform == 'linux2':	    
+if sys.platform == 'linux2':
     LD_LIBRARY_PATH = 'LD_LIBRARY_PATH'
     if not os.environ.has_key( LD_LIBRARY_PATH ) \
        or not set( scons_config.libpath ).issubset( set( os.environ[LD_LIBRARY_PATH].split(':') ) ):
         #see http://hathawaymix.org/Weblog/2004-12-30
         print 'error: LD_LIBRARY_PATH has not been set'
-else:    
+else:
     PATH = os.environ.get( 'PATH', '' )
     PATH=PATH + ';' + ';'.join( scons_config.libpath )
     os.environ['PATH'] = PATH
+
+
+#~ try:
+    #~ import pydsc
+    #~ pydsc.include( r'D:\pygccxml_sources\sources\pyplusplus_dev' )
+    #~ pydsc.ignore( [ 'Yakovenko'
+             #~ , 'Bierbaum'
+             #~ , 'org'
+             #~ , 'http'
+             #~ , 'bool'
+             #~ , 'str'
+             #~ , 'www'
+             #~ , 'param'
+             #~ , 'txt'
+             #~ , 'decl'
+             #~ , 'decls'
+             #~ , 'namespace'
+             #~ , 'namespaces'
+             #~ , 'enum'
+             #~ , 'const'
+             #~ , 'GCC'
+             #~ , 'xcc'
+             #~ , 'TODO'
+             #~ , 'typedef'
+             #~ , 'os'
+             #~ , 'normcase'
+             #~ , 'normpath'
+             #~ , 'scopedef'
+             #~ , 'ira'#part of Matthias mail address
+             #~ , 'uka'#part of Matthias mail address
+             #~ , 'de'#part of Matthias mail address
+             #~ , 'dat'#file extension of directory cache
+             #~ , 'config'#parameter description
+             #~ , 'gccxml'#parameter description
+             #~ , 'Py++'
+             #~ , 'pygccxml'
+             #~ , 'calldef'
+             #~ , 'XXX'
+             #~ , 'wstring'
+             #~ , 'py'
+             #~ ] )
+#~ except ImportError:
+    #~ pass
