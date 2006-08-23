21051d9d438eb7914685037bed80061697336d44
roman_yakovenko <none@none>
2006-08-23 19:32:37 +0000

    moving "create aliases for matchers" code from the module to the declarations package

--- contrib/goodies/dsl_interface.py
+++ contrib/goodies/dsl_interface.py
@@ -22,12 +22,3 @@ from pyplusplus.decl_wrappers import print_declarations
 
 
 # Type traits
-
-# Matchers
-# - Bring in all matchers but rename then without the '_t' at the end
-import pygccxml.declarations.matchers
-for n in ["matcher_base_t","or_matcher_t","and_matcher_t","not_matcher_t",
-          "declaration_matcher_t","calldef_matcher_t","namespace_matcher_t",
-          "variable_matcher_t","regex_matcher_t","access_type_matcher_t",
-          "operator_matcher_t","custom_matcher_t","virtuality_type_matcher_t"]:
-   mod_dict[n[:-2]] = pygccxml.declarations.matchers.__dict__[n]
