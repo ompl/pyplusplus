097db4db801741f6b05c2b1fa21c96ad013ee2e9
allenb <none@none>
2006-08-28 23:35:22 +0000

    Fix mdecls wrappers.

--- contrib/goodies/goodie_overrides.py
+++ contrib/goodies/goodie_overrides.py
@@ -35,13 +35,13 @@ def new_call_redirector_t__call__(self, *arguments, **keywords):
    if not False in [isinstance(x,(pd.declaration_t,pd.mdecl_wrapper_t)) for x in results]:
       all_decls = []
       for x in results:
-         if isinstance( x, declaration.declaration_t ):
+         if isinstance( x, pd.declaration_t ):
             all_decls.append(x)
-         elif isinstance( x, mdecl_wrapper_t ):
-            all_decls.extend(x.decls)
+         elif isinstance( x, pd.mdecl_wrapper_t ):
+            all_decls.extend(x.declarations)
          else:
             assert False, "Should not get here"
-      return mdecl_wrapper_t( all_decls )
+      return pd.mdecl_wrapper_t( all_decls )
    # Otherwise, just return the list
    else:
       return results
@@ -49,7 +49,8 @@ def new_call_redirector_t__call__(self, *arguments, **keywords):
 mdecl_wrapper.call_redirector_t.__call__ = new_call_redirector_t__call__
 
 # Override the mdecl_wrapper_t.__getitem__ method
-
+# - Adds support for calling getitem ( []'s ) on sub decls
+#  ie. ns["Class"]["method"].exclude()  
 def new_mdecl_wrapper_t__getitem__(self,index):
    """Provide access to declaration.
       If passed a standard index, then return contained decl.
