05d25f9a26001e562fe586d617500bec0885afcf
mbaas <none@none>
2006-09-18 13:41:16 +0000

    Added the verbose switch so that epydoc displays markup errors when generating the reference manual for pypp_api.

--- contrib/pypp_api/generate_docs.py
+++ contrib/pypp_api/generate_docs.py
@@ -5,7 +5,7 @@ import os, os.path
 default_format = "epytext"
 
 
-cmd_line = "epydoc -o html --docformat %s pypp_api"%(default_format)
+cmd_line = "epydoc -o html -v --docformat %s pypp_api"%(default_format)
 
 print "Running: ", cmd_line
 os.system(cmd_line)
