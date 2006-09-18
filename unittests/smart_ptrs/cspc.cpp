8a5004a24e19b6aca9ae61d067752b8c3a535bba
roman_yakovenko <none@none>
2006-09-18 21:45:16 +0000

    update smart ptr

--- unittests/smart_ptrs/cspc.cpp
+++ unittests/smart_ptrs/cspc.cpp
@@ -25,6 +25,31 @@ public:
     : pRep(rep), pUseCount( new unsigned int(1) )
 	{}
 
+    smart_ptr_t(const smart_ptr_t& r)
+    : pRep(0), pUseCount(0)
+    {
+		pRep = r.get();
+		pUseCount = r.useCountPointer();
+		if(pUseCount){
+			++(*pUseCount);
+		}
+    }
+
+	smart_ptr_t& operator=(const smart_ptr_t& r){
+		if( pRep == r.pRep ){
+			return *this;
+	    }
+
+		release();
+
+		pRep = r.get();
+		pUseCount = r.useCountPointer();
+		if(pUseCount){
+			++(*pUseCount);
+		}
+		return *this;
+	}
+
     template<class Y>
 	smart_ptr_t(const smart_ptr_t<Y>& r)
     : pRep(0), pUseCount(0)
