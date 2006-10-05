0be7919d864156fde553df292270da1394900c4d
roman_yakovenko <none@none>
2006-10-05 07:43:48 +0000

    replace Py_size_t with ssize_t

--- pyplusplus/code_repository/convenience.py
+++ pyplusplus/code_repository/convenience.py
@@ -32,14 +32,14 @@ raise_error( PyObject *exception, const char *message ){
 }
 
 inline void
-ensure_sequence( boost::python::object seq, Py_ssize_t expected_length=-1 ){
+ensure_sequence( boost::python::object seq, boost::python::ssize_t expected_length=-1 ){
     PyObject* seq_impl = seq.ptr();
 
     if( !PySequence_Check( seq_impl ) ){
         raise_error( PyExc_TypeError, "Sequence expected" );
     }
 
-    Py_ssize_t length = PySequence_Length( seq_impl );
+    boost::python::ssize_t length = PySequence_Length( seq_impl );
     if( expected_length != -1 && length != expected_length ){
         std::stringstream err;
         err << "Expected sequence length is " << expected_length << ". "
@@ -49,11 +49,11 @@ ensure_sequence( boost::python::object seq, Py_ssize_t expected_length=-1 ){
 }
 
 template< class ExpectedType >
-void ensure_uniform_sequence( boost::python::object seq, Py_ssize_t expected_length=-1 ){
+void ensure_uniform_sequence( boost::python::object seq, boost::python::ssize_t expected_length=-1 ){
     ensure_sequence( seq, expected_length );
 
-    Py_ssize_t length = boost::python::len( seq );
-    for( Py_ssize_t index = 0; index < length; ++index ){
+    boost::python::ssize_t length = boost::python::len( seq );
+    for( boost::python::ssize_t index = 0; index < length; ++index ){
         boost::python::object item = seq[index];
 
         boost::python::extract<ExpectedType> type_checker( item );
