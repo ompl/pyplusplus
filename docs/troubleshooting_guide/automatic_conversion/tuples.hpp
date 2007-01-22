f4fd8c29635e2780fc7ba5146ccc086d88373f20
roman_yakovenko <none@none>
2007-01-22 08:46:18 +0000

    adding license

--- docs/troubleshooting_guide/automatic_conversion/tuples.hpp
+++ docs/troubleshooting_guide/automatic_conversion/tuples.hpp
@@ -1,3 +1,8 @@
+// Copyright 2004-2007 Roman Yakovenko.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef TUPLES_HPP_16_JAN_2007
 #define TUPLES_HPP_16_JAN_2007
 
