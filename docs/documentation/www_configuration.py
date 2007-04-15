073cb77b3cfec538eb99d0c14899943cd0826f43
roman_yakovenko <none@none>
2007-04-15 19:34:49 +0000

    improving docs

--- docs/documentation/www_configuration.py
+++ docs/documentation/www_configuration.py
@@ -1,6 +1,6 @@
 name = 'documentation'
 main_html_file = 'index.html'
-files_to_skip = ['indexing_suite_v2.html']
+files_to_skip = ['indexing_suite_v2.html', 'feedback.html']
 names = { 'containers' : 'STL containers'
           , 'how_to' : 'how to ... ?'
           , 'doc_string' : 'documentation string'
