59299eff76675271d267f9d97b832ebde48f40ab
roman_yakovenko <none@none>
2007-04-25 05:53:58 +0000

    updating version in setup.py files

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ if sys.argv[-1]=="sdist":
 
 
 setup( name = "Py++",
-       version = "0.8.6",
+       version = "0.9.0",
        description="Py++ is a framework of components for creating C++ code generator for Boost.Python library",
        author="Roman Yakovenko",
        author_email="roman.yakovenko@gmail.com",
