d4995aa74ad717d5f41dbb141ea5482015d1f96a
roman_yakovenko <none@none>
2008-01-05 20:47:42 +0000

    improving docs

---
+++ docs/documentation/how_to/www_configuration.py
@@ -0,0 +1,10 @@
+name = 'how to ... ?'
+#main_html_file = 'index.html'
+
+names = { 'hints' : 'hints'
+          , 'how_to' : 'how to'
+          , 'templates' : 'deal with templates'
+          , 'best_practices' : 'best practices'
+          , 'exception_translation' : 'exception translation'
+}
+
