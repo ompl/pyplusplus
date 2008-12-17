28a97d0032630c38f45f2752fa58527d846eeefd
roman_yakovenko <none@none>
2008-12-17 21:59:33 +0000

    adding ctypes-cpp tester - remove unnecessary files

--- pyplusplus/cpptypes/decorators.py
+++
@@ -1,9 +0,0 @@
-class public( object ):
-    def __init__(self ,  dll,  decorated_name,  return_type,  argumen_types):
-        self.decorated_name = decorated_name
-        self.func = getattr( dll,  decorated_name )
-        self.func.restype = return_type
-        self.func.argtypes = argumen_types
-
-    def __call__(self, *args, **keywd ):
-        return self.func( *args,  **keywd )
