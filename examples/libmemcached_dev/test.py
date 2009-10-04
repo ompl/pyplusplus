3905814d12347a566de87b282c88b54a8f1bd4eb
roman_yakovenko <none@none>
2009-10-04 19:59:05 +0000

    update libmemcached example

--- examples/libmemcached_dev/test.py
+++ examples/libmemcached_dev/test.py
@@ -16,3 +16,12 @@ print "key/value (%s/%s ) was stored?: " % ( key, value ) + mmc.memcached_strerr
 
 result = mmc.memcached_delete( memc, key, len(key), 0);
 print "key/value (%s/%s ) was deleted?: " % ( key, value ) + mmc.memcached_strerror(memc, result);
+
+for key in 'abcdefghijklmnopqrstuvwxyz':
+    value_length = ctypes.c_size_t()
+    flags = ctypes.c_ulong()
+    result = mmc.memcached_return()
+    value = mmc.memcached_get( memc, key, len(key), ctypes.byref( value_length ), ctypes.byref( flags ), ctypes.byref( result ) )
+    print 'key/value (%s/%s)' % ( key, value )
+    print   mmc.memcached_strerror(memc, result);
+
